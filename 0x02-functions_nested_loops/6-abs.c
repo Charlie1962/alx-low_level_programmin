f2ed8d1eac09e398eabe7ce507e62de7fbea9268
Charlie1962 <emeanuacharles@gmail.com>
2022-09-13 21:44:00 -0700

    my commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,15 @@
 #include "main.h"
-/**
-  * _abs - The entry point of the function
-  * @x: The argument to be evaluated
-  *
-  * Description: This is a function that computes the absolute value of an
-  * integer.
-  * Return: abs_val
-  */
-int _abs(int x)
-{
-	int abs_val;
 
-	if (n < 0)
-		abs_val = -1 * n;
-	else
-		abs_val = n;
-	return (abs_val);
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
-  * print_last_digit - The entry point of the function
-  * @r: The number to be checked
-  *
-  * Description: This is a function that prints the last digit of a number.
-  * Return: last_digt
-  */
-int print_last_digit(int r)
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
 {
-	int last_digt;
+	int a;
 
 	if (n < 0)
-		last_digt = (n * -1) % 10;
-	else
-		last_digt = n % 10;
-	_putchar(last_digt + '0');
-	return (last_digt);
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
